fbc7a339f55c9f0de812973fe36b3472b0788aec
hopes91 <nadya.me91@gmail.com>
2019-04-20 13:49:15 +0300

    refactor

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-var css = document.querySelector('h3');
 var color1 = document.querySelector('.color1');
 var color2 = document.querySelector('.color2');
 var body = document.getElementById('gradient');
-var button = document.querySelector('button'); 
+var button = document.querySelector('button');
 
 function setColorsAndText() {
+	var css = document.querySelector('h3');
+
 	body.style.background = 'linear-gradient(to right, ' + color1.value + ', ' + color2.value + ')';
 	css.textContent = body.style.background + ';';
 }
@@ -12,15 +13,18 @@ function setColorsAndText() {
 function getRandomColor() {
 	var letters = '0123456789ABCDEF'.split('');
 	var color = '#';
-	for(var i = 0; i < 6; i++) {
-		color += letters[Math.round(Math.random() * 15)];
+
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.round(Math.random() * 16)];
 	}
+
 	return color;
 }
 
 function setRandomColors() {
 	color1.value = getRandomColor();
 	color2.value = getRandomColor();
+	
 	setColorsAndText();
 }
 
